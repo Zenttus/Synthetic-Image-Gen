1870a79ae2acc2f769bbc2d0ec94dd7ccaa96e38
Pedro Santiago <pedro.santiago16@upr.edu>
2018-09-27 10:36:54 -0400

    Initial commit of datagen script

--- datagen.py
+++ datagen.py
@@ -2,20 +2,21 @@ import bpy
 import os
 
 pathToBackgroundImages = os.path.join('C:\\', 'Users', 'Owrn', 'Desktop', 'backgroundImages')
-cameraConstant = 2.1892349261
+cameraConstant = 2.1892349261 #Based on the camera angle(if field of view changes this must also change).
 
 #Clean up scene
 for obj in bpy.context.scene.objects:
     obj.select = True
     bpy.ops.object.delete(use_global=False)
-    
+
+#Create Camera
 bpy.ops.object.camera_add(view_align=True, enter_editmode=False, location=(0,0,0), rotation=(0,0,0))
 camera = bpy.context.object
 
+#Get Images
 fileList = os.listdir(pathToBackgroundImages)
 jpgList = [item for item in fileList if item[-3:] == 'jpg']
 #print(jpgList)
-
 
 
 for img in jpgList:
