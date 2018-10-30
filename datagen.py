9f7c02e9aa3d4c16dd1c5949e044f4a9e313d8b7
Pedro Santiago <pedro.santiago16@upr.edu>
2018-10-30 04:37:14 -0400

    move some sections of code into functions, added todos for organization

--- datagen.py
+++ datagen.py
@@ -1,7 +1,8 @@
 import bpy
 import os
 import random
-    
+
+#TODO move functions to library and add cocumentation
 #TODO check valididity of zrange(zrange being the distance from image)
 def posObjRnd(object, zrange, relativeSize, imageX):
      zPos = random.uniform(0,zrange)
@@ -10,35 +11,43 @@ def posObjRnd(object, zrange, relativeSize, imageX):
      print(zPos)
      print(yPos)
      print(xPos)
-     bpy.ops.mesh.primitive_uv_sphere_add(location = (xPos, yPos, zPos), size=0.1)
+     bpy.ops.mesh.primitive_uv_sphere_add(location = (xPos, yPos, zPos), size=0.1)#TODO change this to load data
 
-#TODO fix the backgournd image path(make them arguments)
-pathToBackgroundImages = os.path.join('C:\\', 'Users', 'Owrn', 'Documents', 'gitRepos', 'synthetic-data-gen', 'backgroundImages')
-cameraConstant = 2.1892349261 #Based on the camera angle(if field of view changes this must also change).
+def cleanScene():
+    for obj in bpy.context.scene.objects:
+        obj.select = True
+        bpy.ops.object.delete(use_global=False)
 
-#Clean up scene
-for obj in bpy.context.scene.objects:
-    obj.select = True
-    bpy.ops.object.delete(use_global=False)
+def loadImages():#TODO add args
+    #TODO make it read from args
+    #pathToBackgroundImages = sys.argv[1]
+    pathToBackgroundImages = os.path.join('C:\\', 'Users', 'Owrn', 'Documents', 'gitRepos', 'synthetic-data-gen', 'backgroundImages')
+    picList = os.listdir(pathToBackgroundImages)
+    jpgList = [item for item in picList if item[-3:] == 'jpg']
+    return picList
+
+def changeImage(camera, img):
+    bpy.ops.import_image.to_plane(files=[{"name":img}], location=(0,0,0), directory=pathToBackgroundImages,shader='SHADELESS', relative=False)
+    temp = bpy.context.object
+    temp.location = (0,0,0)
+    camera.location = (0,0,abs(temp.data.vertices[0].co[0])*cameraConstant)
+    return temp.data.vertices[0].co[0]
+
+
+
+pathToBackgroundImages = os.path.join('C:\\', 'Users', 'Owrn', 'Documents', 'gitRepos', 'synthetic-data-gen', 'backgroundImages')#TODO remove this eventually
+cameraConstant = 2.1892349261 #Based on the camera angle(if field of view changes this must also change)
+
+cleanScene()
 
 #Create Camera
 bpy.ops.object.camera_add(view_align=True, enter_editmode=False, location=(0,0,0), rotation=(0,0,0))
 camera = bpy.context.object
 
-#Get Images
-fileList = os.listdir(pathToBackgroundImages)
-jpgList = [item for item in fileList if item[-3:] == 'jpg']
-#print(jpgList)
+#TODO add light source
 
+jpgList = loadImages()
 
 for img in jpgList:
-    bpy.ops.import_image.to_plane(files=[{"name":img}], location=(0,0,0), directory=pathToBackgroundImages,shader='SHADELESS', relative=False)
-    temp = bpy.context.object
-    temp.location = (0,0,0)
-    camera.location = (0,0,abs(temp.data.vertices[0].co[0])*cameraConstant)
-    print(temp.data.vertices[0].co)
-    posObjRnd(camera, 1, 1, temp.data.vertices[0].co[0]) 
-
-     
-    
-    
+    imagePos = changeImage(camera, img)
+    posObjRnd(camera, 1, 1, imagePos)
