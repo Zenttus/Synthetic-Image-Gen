4027c3663152821f80060a885464f280f246cc10
Pedro Santiago <pedro.santiago16@upr.edu>
2018-11-14 08:22:09 -0400

    Clean up code.

--- datagen.py
+++ datagen.py
@@ -1,19 +1,28 @@
 import bpy
 import os
+import time
 import sys
-import toolbox
 import json
 import random
+import toolbox
 
 #TODO that the conf file has the backgroundImages info
+#TODO script to set shits
+#TODO Make background black
+#TODO Make script to automate setting up
+#TODO dinamic camera
+#TODO label type
+#TODO Write README
+#TODO Make object a class
+
+tick = time.time()
 
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 # TODO clean this mess
-# TODO : Make it for more than one type of object
 pathToBackgroundImages = config['SETTINGS']['BackGroundImagesPath']
-pathToObject = config['OBJECTS']['O1']['ObjectModelPath']
+pathToObject = config['OBJECTS']['O1']['ObjectModelPath'] # TODO : Make it for more than one type of object
 pathToResults = config['SETTINGS']['PathToResults']
 numOfPics = config['SETTINGS']['PicturesPerBackgroudn']
 resX = config['SETTINGS']['ResX']
@@ -22,10 +31,16 @@ label = config['OBJECTS']['O1']['Label']
 rotate = config['OBJECTS']['O1']['Rotate']
 maxNumOfObjects = config['OBJECTS']['O1']['MaxRepetition']
 alp = config['OBJECTS']['O1']['Alpha']
+usingGPU = config['SETTINGS']['GPU']
+xmlLabels = config['SETTINGS']
 
 #Prepare enviorment
 scene, camera, lamp = toolbox.prepareScene(resX, resY)
 jpgList = toolbox.loadImages(pathToBackgroundImages)
+
+#GPU acceleration
+if(usingGPU==1):
+    scene.cycles.device = 'GPU'
 
 object = [None] * maxNumOfObjects
 currentImg = None
@@ -36,6 +51,10 @@ for imgName in jpgList:
         object = toolbox.posObjRnd(object, scene, camera, 1, 1, imagePos, pathToObject, rotation=rotate, alpha=alp)
         lamp = toolbox.updateLamp(lamp, scene, camera)
 
-        bpy.data.scenes['Scene'].render.filepath = pathToResults + '\\' + str(imgName[:-4]) + str(n) + '.png' # TODO find best format.
+        bpy.data.scenes['Scene'].render.filepath = pathToResults + '\\' + str(imgName[:-4]) + str(n) + '.jpg' # TODO find best format.
         bpy.ops.render.render( write_still=True )
-        toolbox.generateLabelFile(object, scene, camera, pathToResults, str(imgName[:-4]) + str(n), resX, resY, label)
+        toolbox.generateLabelTextFile(object, scene, camera, pathToResults, str(imgName[:-4]) + str(n), resX, resY, label)
+        #toolbox.generateLabelFile(object, scene, camera, pathToResults, str(imgName[:-4]) + str(n), resX, resY, label)
+
+tock = time.time()
+print("Pictures Printed: " + str(len(jpgList)*numOfPics) + "\nResolution: " + str(resX) + " x " + str(resY) + "\nTime: " + str(tock-tick))
--- toolbox.py
+++ toolbox.py
@@ -7,9 +7,78 @@ from lxml import etree
 # TODO: Documentation
 # TODO: Oganize order
 # TODO: Make parameters name better
-# TODO: Different label modes for different algorithms
+# TODO: Look and implement label modes for different algorithms
+# TODO: Write object types on doc strings
 
-# TODO: Incorporate scale and origin, different types of file, random rotation
+def prepareScene(resX, resY):
+    '''
+    Initiacion of scene(creates camera and lamp)
+    '''
+    #Clear all
+    for obj in bpy.context.scene.objects:
+        obj.select = True
+        bpy.ops.object.delete(use_global=False)
+    #Create Camera
+    bpy.ops.object.camera_add(view_align=True, enter_editmode=False, location=(0,0,0), rotation=(0,0,0))
+    camera = bpy.context.object
+    scene = bpy.context.scene
+    scene.camera = bpy.context.object
+    scene.render.resolution_x = resX
+    scene.render.resolution_y = resY
+    scene.render.resolution_percentage = 100
+    #create light
+    lamp = updateLamp(None, scene, camera)
+
+    return scene, camera, lamp
+
+# TODO: Position the lamp and color it based on backgroundImage.
+def updateLamp(lamp, scene, img):
+    '''
+    Randomizes lamp position, if it doesnt exist it creates a lamp.
+    '''
+    #Create lamp if it doesnt exist.
+    if(lamp==None):
+        lamp_data = bpy.data.lamps.new(name="New Lamp", type='POINT')
+        lamp = bpy.data.objects.new(name="New Lamp", object_data=lamp_data)
+        scene.objects.link(lamp)
+
+    xPos = random.uniform(img.location[0] - 2, img.location[0] + 2)
+    yPos = random.uniform(img.location[1] - 2, img.location[1] + 2)
+    zPos = random.uniform(img.location[2] + 1, img.location[2] + 2)
+
+    lamp.location = (xPos, yPos, zPos)
+    lamp.select = True
+    scene.objects.active = lamp
+
+    #Randomize Color
+    r = random.uniform(.5,1)
+    g = random.uniform(.5,1)
+    b = random.uniform(.5,1)
+    lamp.data.color = [r, g, b]
+
+    return lamp
+
+# TODO: make positioning of camera more accurate(so that there's no black bars)
+def changeImage(camera, newImgName, oldImg, pathToBackgroundImages):
+    '''
+    Changes the background Image.
+    '''
+    cameraConstant = 2.1892349261 #Based on the camera angle(if field of view changes this must also change) # TODO: get this via code.
+    deselectAll()
+    #Delete Old Image(if there's one)
+    if(oldImg!=None):
+        oldImg.select = True
+        bpy.ops.object.delete(use_global=False)
+    #loadNewImage
+    bpy.ops.import_image.to_plane(files=[{"name":newImgName}], location=(0,0,0), directory=pathToBackgroundImages,shader='SHADELESS', relative=False)
+    newImg = bpy.context.object
+    newImg.location = (0,0,0)
+    imgPos = newImg.data.vertices[0].co[0]
+
+    camera.location = (0,0,abs(imgPos)*cameraConstant)
+    return imgPos, newImg
+
+# TODO: Incorporate scale and origin, different types of file
 def posObjRnd(object, scene, camera, zrange, relativeSize, imageX, filePath, alpha=0.5, rotation=1):
 
     if(object[0]==None):
@@ -46,7 +115,6 @@ def posObjRnd(object, scene, camera, zrange, relativeSize, imageX, filePath, alp
 def makeInvisible(ob):
     ob.location = (0,0,-20) # TODO : This is not the best way, fix this(imagine an object that is that big)
 
-
 def copyObjects(arrayOfObjects, obj, scene):
     arrayOfObjects[0] = obj
     for n in range(1, len(arrayOfObjects)):
@@ -58,68 +126,61 @@ def deselectAll():
     for obj in bpy.data.objects:
         obj.select = False
 
-def prepareScene(resX, resY):
-    #Clear all
-    for obj in bpy.context.scene.objects:
-        obj.select = True
-        bpy.ops.object.delete(use_global=False)
-    #Create Camera
-    bpy.ops.object.camera_add(view_align=True, enter_editmode=False, location=(0,0,0), rotation=(0,0,0))
-    camera = bpy.context.object
-    scene = bpy.context.scene
-    scene.camera = bpy.context.object
-    scene.render.resolution_x = resX
-    scene.render.resolution_y = resY
-    scene.render.resolution_percentage = 100
-    #create light
-    lamp = updateLamp(None, scene, camera)
-
-    return scene, camera, lamp
-
+#Update this to get them from the conf file.
 def loadImages(backgroundImagesPath):
     picList = os.listdir(backgroundImagesPath)
-    jpgList = [item for item in picList if item[-3:] == 'jpg']
+    jpgList = [item for item in picList if item[-3:] == 'jpg'] #TODO: add other kinds of images
     return picList
 
-def changeImage(camera, newImgName, oldImg, pathToBackgroundImages):
-    cameraConstant = 2.1892349261 #Based on the camera angle(if field of view changes this must also change)
-    deselectAll()
-    #Delete Old Image(if there's one)
-    if(oldImg!=None):
-        oldImg.select = True
-        bpy.ops.object.delete(use_global=False)
-    #loadNewImage
-    bpy.ops.import_image.to_plane(files=[{"name":newImgName}], location=(0,0,0), directory=pathToBackgroundImages,shader='SHADELESS', relative=False)
-    newImg = bpy.context.object
-    newImg.location = (0,0,0)
-    imgPos = newImg.data.vertices[0].co[0]
-    camera.location = (0,0,abs(imgPos)*cameraConstant)
-    return imgPos, newImg
+# TODO: Implement mode to save object distance
+def getObjCords(scene, obj, camera, resX, resY):
+    """
+    Returns the coordinates that encloses an object in image.
+    """
 
-# TODO: make it more random(color intensity)
-def updateLamp(lamp, scene, img):
-    #Create lamp if it doesnt exist.
-    if(lamp==None):
-        lamp_data = bpy.data.lamps.new(name="New Lamp", type='POINT')
-        lamp = bpy.data.objects.new(name="New Lamp", object_data=lamp_data)
-        scene.objects.link(lamp)
-    xPos = random.uniform(img.location[0] - 2, img.location[0] + 2)
-    yPos = random.uniform(img.location[1] - 2, img.location[1] + 2)
-    zPos = random.uniform(img.location[2] + 1, img.location[2] + 2)
+    #Get vertices
+    mw = obj.matrix_world
+    verts = (mw * vert.co for vert in obj.data.vertices)
+    cords2d = [world_to_camera_view(scene, camera, coord) for coord in verts]
 
-    lamp.location = (xPos, yPos, zPos)
-    lamp.select = True
-    scene.objects.active = lamp
+    xmax = round(cords2d[0][0]*resX)
+    xmin = round(cords2d[0][0]*resX)
+    ymax = round(cords2d[0][1]*resY)
+    ymin = round(cords2d[0][1]*resY)
 
-    #Randomize Color 
-    r = random.uniform(.5,1)
-    g = random.uniform(.5,1)
-    b = random.uniform(.5,1)
-    lamp.data.color = [r, g, b]
+    #Calculate min and max of vertices.
+    for x, y, d in cords2d:
+        if(xmax<x*resX):
+            xmax = round(x * resX)
+        if(xmin>x*resX):
+            xmin = round(x * resX)
+        if(ymax<y*resY):
+            ymax = round(y * resY)
+        if(ymin>y*resY):
+            ymin = round(y * resY)
 
-    return lamp
+    #If object has part out of image.
+    if(xmin<0):
+        xmin = 0
+    if(ymin<0):
+        ymin = 0
+    if(xmax>resX):
+        xmax = resX
+    if(ymax>resY):
+        ymax = resY
 
-def generateLabelFile(object, scene, camera, pathToResults, img, resX, resY, Objectlabel):
+    #Convert coordinate system #TODO: make this a parameter, to have different coordinates modes.
+    ymax = resY - ymax
+    ymin = resY - ymin
+
+    return xmax, xmin, ymin, ymax
+
+# LABELING FUNCTIONS
+
+def generateLabelXmlFile(object, scene, camera, pathToResults, img, resX, resY, Objectlabel):
+    '''
+    Generate the .xml version of the label file.
+    '''
     root = etree.Element("annotation")
 
     folder = etree.SubElement(root, "folder")
@@ -170,39 +231,16 @@ def generateLabelFile(object, scene, camera, pathToResults, img, resX, resY, Obj
     file.write(str(etree.tostring(root, pretty_print=True)).replace("\\n","")[2:-1])
     file.close()
 
-# TODO: Research of something to be done with the distance parameter?
-def getObjCords(scene, obj, camera, resX, resY):
-    mw = obj.matrix_world
+def generateLabelTextFile(object, scene, camera, pathToResults, img, resX, resY, Objectlabel):
+    '''
+    Generate the .txt version of the label file.
+    '''
+    file = open(pathToResults + '\\' + img + '.txt', 'w')
+    for o in object:
+        if(o.hide == True):
+            continue
+        xmaxV, xminV, yminV, ymaxV = getObjCords(scene, o, camera, resX, resY)
+        s = str(Objectlabel) + ' ' + str(((xmaxV+xminV)/2)/resX) + ' ' + str(((ymaxV+yminV)/2)/resY) + ' ' + str((xmaxV-xminV)/resX) + ' ' + str(abs(ymaxV-yminV)/resY) + '\n'
+        file.write(s)
 
-    verts = (mw * vert.co for vert in obj.data.vertices)
-    cords2d = [world_to_camera_view(scene, camera, coord) for coord in verts]
-
-    xmax = round(cords2d[0][0]*resX)
-    xmin = round(cords2d[0][0]*resX)
-    ymax = round(cords2d[0][1]*resY)
-    ymin = round(cords2d[0][1]*resY)
-
-    for x, y, d in cords2d:
-        if(xmax<x*resX):
-            xmax = round(x * resX)
-        if(xmin>x*resX):
-            xmin = round(x * resX)
-        if(ymax<y*resY):
-            ymax = round(y * resY)
-        if(ymin>y*resY):
-            ymin = round(y * resY)
-
-    if(xmin<0):
-        xmin = 0
-    if(ymin<0):
-        ymin = 0
-
-    if(xmax>resX):
-        xmax = resX
-    if(ymax>resY):
-        ymax = resY
-
-    ymax = resY - ymax
-    ymin = resY - ymin
-
-    return xmax, xmin, ymin, ymax
+    file.close()
